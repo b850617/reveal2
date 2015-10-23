28d243aa3bb651eec8fef52c72eb84b0c5f21ef4
40323250 <b850617@yahoo.com.tw>
2015-10-23 19:23:31 +0800

    blog list

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 四設二乙  
+<p><a href="http://b850617.github.io/2015cpHW/">My Github Blog</a></p>
 <p><a href="http://b850617.github.io/reveal2/">My Github Blog_2</a></p>
+<p><a href="http://2015fallhw.github.io/2015fallcadpb/user/40323250/">Class homework page</a></p>
--- pelican/publishconf.py
+++ pelican/publishconf.py
@@ -25,13 +25,13 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
+LINKS = (('班上共同倉儲作業', 'http://2015fallhw.github.io/2015fallcadpb/user/40323250/'),('reveal_1', 'http://b850617.github.io/2015cpHW/'),('2015課程網頁', 'http://wordpress-2015course.rhcloud.com/'),
          ('Python', 'http://python.org/'),)
 
 # Social widget
 SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
           ('github', 'http://github.com/ametaireau'),
-     ('reveal_1', 'http://b850617.github.io/2015cpHW/'),)
+     )
           
 DEFAULT_PAGINATION = 10
 
